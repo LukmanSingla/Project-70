146e03d8bebd5a844d90ee4f0b788639d6a592ec
LukmanSingla <79403472+LukmanSingla@users.noreply.github.com>
2021-10-07 14:16:45 +0530

    Update ReadStoryScreen.js

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,21 +1,24 @@
- import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default class ReadStoryScreen extends React.Component {
-    render(){
-        return(
-            <View style={styles.container}>
-                <Text>Read Story</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Text>Read Story</Text>
+        </View>
+      </SafeAreaProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
